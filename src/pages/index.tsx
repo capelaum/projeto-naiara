d4804a44dd80eea86d3ed4752b942c55fcde3bb1
capelaum <thecapellett@gmail.com>
2024-06-14 11:49:17 -0300

    fix: remove instagram widget

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import 'keen-slider/keen-slider.min.css'
-import dynamic from 'next/dynamic'
 import Head from 'next/head'
-import { ThreeDots } from 'react-loader-spinner'
 import { Navbar } from '~/components/Navbar'
 import { About } from '~/sections/About'
 import { Appointment } from '~/sections/Appointment'
@@ -12,23 +10,23 @@ import { Services } from '~/sections/Services'
 import { Testimonials } from '~/sections/Testimonials'
 import { HomeMain } from '~/styles/home'
 
-const Instagram = dynamic(
-  () => import('~/sections/Instagram').then((res) => res.Instagram),
-  {
-    ssr: false,
-    loading: () => (
-      <ThreeDots
-        height="80"
-        width="80"
-        radius="9"
-        color="#963F33"
-        ariaLabel="three-dots-loading"
-        wrapperStyle={{}}
-        visible={true}
-      />
-    ),
-  },
-)
+// const Instagram = dynamic(
+//   () => import("~/sections/Instagram").then((res) => res.Instagram),
+//   {
+//     ssr: false,
+//     loading: () => (
+//       <ThreeDots
+//         height="80"
+//         width="80"
+//         radius="9"
+//         color="#963F33"
+//         ariaLabel="three-dots-loading"
+//         wrapperStyle={{}}
+//         visible={true}
+//       />
+//     ),
+//   }
+// );
 
 export default function Home() {
   return (
@@ -45,7 +43,7 @@ export default function Home() {
         <Services />
         <About />
         <Testimonials />
-        <Instagram />
+        {/* <Instagram /> */}
         <Contact />
       </HomeMain>
 
